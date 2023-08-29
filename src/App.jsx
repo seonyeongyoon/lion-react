3981bd49e40db0da7b3f68a8cca1af19ec6081f8
seonyeongyoon <ssunnyy0204@gmail.com>
2023-08-29 18:52:13 +0900

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import AuthProvider from '@/contexts/Auth';
 import ThemeProvider from '@/contexts/Theme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { HelmetProvider } from 'react-helmet-async';
 import { Toaster } from 'react-hot-toast';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
+
+//쿼리 클라이언트 객체 생성
+const queryClient = new QueryClient(); //모든 쿼리에 사용되는 기본 옵션
+
+//쿼리 클라이언트를 앱에 공급
 
 function App() {
   return (
@@ -11,9 +18,12 @@ function App() {
       <HelmetProvider>
         <ThemeProvider>
           <AuthProvider>
-            <div className="App">
-              <RouterProvider router={router} />
-            </div>
+            <QueryClientProvider client={queryClient}>
+              <div className="App">
+                <RouterProvider router={router} />
+              </div>
+              <ReactQueryDevtools/>
+            </QueryClientProvider>
           </AuthProvider>
         </ThemeProvider>
       </HelmetProvider>
